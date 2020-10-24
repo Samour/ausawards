ae6c0ef571a181ba5298b4504c2549062c5d8408
Aaron <aaron@Edvals-MacBook-Air.local>
2020-10-24 11:48:30 +1100

    Implemented healthcheck command

--- cli/src/commands/healthcheck.rs
+++ cli/src/commands/healthcheck.rs
@@ -1,6 +1,13 @@
 use crate::args::ArgsParser;
 use crate::commands::Command;
 use crate::domain::Config;
+use reqwest;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Response {
+  name: String,
+}
 
 pub struct HealthCheckCommand {}
 
@@ -11,7 +18,22 @@ impl HealthCheckCommand {
 }
 
 impl Command for HealthCheckCommand {
-  fn execute(&self, _: ArgsParser, config: Config) {
-    println!("Execute healthcheck against {}", config.remote.url);
+  fn execute(&self, _: ArgsParser, config: Config) -> () {
+    let base_url = reqwest::Url::parse(&config.remote.url);
+    if base_url.is_err() {
+      println!("Malformed URL: {}", config.remote.url);
+      return;
+    }
+    let base_url = base_url.unwrap();
+
+    let response = reqwest::blocking::get(base_url.join("/health").unwrap())
+      .ok()
+      .map(|r| r.json::<Response>().ok())
+      .flatten();
+    if let Some(r) = response {
+      println!("Successfully connected to {}", r.name);
+    } else {
+      println!("Could not connect to server");
+    }
   }
 }
